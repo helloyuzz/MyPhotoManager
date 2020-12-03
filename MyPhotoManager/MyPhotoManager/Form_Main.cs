b621b56e391488b86fbef0afa9c4581ed133a1e0
helloyuzz <39078847+helloyuzz@users.noreply.github.com>
2020-12-03 17:09:26 +0800

    Update Form_Main.cs

--- MyPhotoManager/MyPhotoManager/Form_Main.cs
+++ MyPhotoManager/MyPhotoManager/Form_Main.cs
@@ -1,13 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Configuration;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Microsoft.WindowsAPICodePack.Shell;
 
 namespace MyPhotoManager {
     public partial class Form_Main:Form {
@@ -17,6 +21,19 @@ namespace MyPhotoManager {
 
         private void Form_Main_Load(object sender,EventArgs e) {
             init_Disk();
+            load_Config();
+        }
+
+        private void load_Config() {
+            string pk_DefaultDisk = ConfigurationManager.AppSettings["DefaultDisk"];
+            string pk_SearchAllDirectories = ConfigurationManager.AppSettings["SearchAllDirectories"];
+            string pk_PhotoFileType = ConfigurationManager.AppSettings["PhotoFileType"];
+
+            if(string.IsNullOrEmpty(pk_DefaultDisk) == false) {
+                cbx_Disk.Text = pk_DefaultDisk;
+            }
+            cbx_SearchAllDirectory.Checked = Boolean.Parse(pk_SearchAllDirectories);
+            tbx_searchPattern.Text = pk_PhotoFileType;
         }
 
         private void init_Disk() {
@@ -41,47 +58,147 @@ namespace MyPhotoManager {
         }
 
         private void cbx_Disk_SelectedIndexChanged(object sender,EventArgs e) {
-            tvw_DirectoryTree.Nodes.Clear();
-            string diskPath = cbx_Disk.Text;
-            if(Directory.Exists(diskPath) == false) {
-                return;
+            load_SubDirectory(null,cbx_Disk.Text);
+            load_SubPhoto(cbx_Disk.Text);
+            save_Config("DefaultDisk",cbx_Disk.Text);
+        }
+
+        private void save_Config(string key,string value) {
+            Configuration configuration = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            configuration.AppSettings.Settings[key].Value = value;
+            configuration.Save(ConfigurationSaveMode.Modified);
+        }
+
+        private void load_SubDirectory(TreeNode treeNode,string targetPath = null) {            
+            tsl_CountTip.Text = "";
+            tsl_ErrorLog.Text = "";            
+
+            if(treeNode == null) {
+                tvw_DirectoryTree.Nodes.Clear();
+                if(Directory.Exists(targetPath) == false) {
+                    return;
+                }
+            } else {
+                targetPath = treeNode.Name;
+                treeNode.Nodes.Clear();
             }
-            string[] directories = Directory.GetDirectories(diskPath);
-            foreach(string dirPath in directories) {
-                DirectoryInfo dirInfo = new DirectoryInfo(dirPath);
-                bool isHidden = false;
+
+            // 加载下级文件夹
+            string[] subDirectories = Directory.GetDirectories(targetPath);
+            foreach(string subDirPath in subDirectories) {
+                DirectoryInfo dirInfo = new DirectoryInfo(subDirPath);
                 bool isDirectory = false;
 
                 //判断是否隐藏文件夹
                 if((dirInfo.Attributes & FileAttributes.Hidden) == FileAttributes.Hidden) {
-                    isHidden = true;
-                }
-                if(isHidden == true) {
                     continue;
                 }
-
+                if((dirInfo.Attributes & FileAttributes.System) == FileAttributes.System) {
+                    continue;
+                }
                 // 判断类型是文件或文件夹
                 if((dirInfo.Attributes & FileAttributes.Directory) == FileAttributes.Directory) {
                     isDirectory = true;
                 }
 
-                TreeNode treeNode = new TreeNode();
+                TreeNode newNode = new TreeNode();
 
-                treeNode.Name = dirInfo.FullName;
-                treeNode.Text = dirInfo.Name;
-                treeNode.ImageIndex = isDirectory ? 1 : 0;
-                treeNode.SelectedImageIndex = treeNode.ImageIndex;
+                newNode.Name = dirInfo.FullName;
+                newNode.Text = dirInfo.Name;
+                newNode.ImageIndex = isDirectory ? 1 : 0;
+                newNode.SelectedImageIndex = newNode.ImageIndex;
 
-                tvw_DirectoryTree.Nodes.Add(treeNode);
+                if(treeNode == null) {
+                    tvw_DirectoryTree.Nodes.Add(newNode);
+                } else {
+                    treeNode.Nodes.Add(newNode);
+                }
+            }
+            if(treeNode != null) {
+                treeNode.Expand();
+                tsl_CountTip.Text = "文件夹：" + treeNode.Nodes.Count;
             }
         }
 
-        private void tvw_DirectoryTree_AfterSelect(object sender,TreeViewEventArgs e) {
-            loadPhotoByDirectory(e.Node.Name);
+        private void toolMenu_OpenDirectory_Click(object sender,EventArgs e) {
+            if(tvw_DirectoryTree.SelectedNode == null) {
+                return;
+            }
+            Process.Start(tvw_DirectoryTree.SelectedNode.Name);
         }
 
-        private void loadPhotoByDirectory(string dirPath) {
-            
+        private void tvw_DirectoryTree_NodeMouseClick(object sender,TreeNodeMouseClickEventArgs e) {
+            load_SubPhoto(e.Node.Name);
+        }
+
+        private void tvw_DirectoryTree_NodeMouseDoubleClick(object sender,TreeNodeMouseClickEventArgs e) {
+            if(e.Node.Nodes.Count > 0) {
+                return;
+            }
+            load_SubDirectory(e.Node);
+        }
+
+        private void load_SubPhoto(string directoryPath) {
+            SearchOption searchOption = cbx_SearchAllDirectory.Checked ? SearchOption.AllDirectories : SearchOption.TopDirectoryOnly;
+            string searchPattern = tbx_searchPattern.Text;
+
+            dgv_Main.Rows.Clear();
+            // 加载文件夹中的照片
+            try {
+                string[] files = Directory.GetFiles(directoryPath,"*.bmp",searchOption)
+                    .Union(Directory.GetFiles(directoryPath,"*.gif",searchOption))
+                    .Union(Directory.GetFiles(directoryPath,"*.png",searchOption))
+                    .Union(Directory.GetFiles(directoryPath,"*.jpg",searchOption))
+                    .Union(Directory.GetFiles(directoryPath,"*.jpeg",searchOption))
+                    .ToArray();
+                foreach(string filePath in files) {
+                    FileInfo fileInfo = new FileInfo(filePath);
+                    dgv_Main.Rows.Add();
+                    dgv_Main.Rows[dgv_Main.Rows.Count - 1].Cells["name_Column"].Value = fileInfo.Name;
+                    dgv_Main.Rows[dgv_Main.Rows.Count - 1].Cells["size_Column"].Value = fileInfo.Length;
+                    dgv_Main.Rows[dgv_Main.Rows.Count - 1].Cells["type_Column"].Value = fileInfo.Extension;
+                    dgv_Main.Rows[dgv_Main.Rows.Count - 1].Cells["modifyTime_Column"].Value = fileInfo.LastWriteTime.ToString("yyyy-MM-dd HH:mm:ss");
+                    dgv_Main.Rows[dgv_Main.Rows.Count - 1].Cells["path_Column"].Value = fileInfo.DirectoryName;
+                }
+            } catch(Exception exc) {
+                tsl_ErrorLog.Text = exc.Message;
+            }
+            tsl_CountTip.Text += "，照片文件：" + dgv_Main.Rows.Count;
+        }
+
+        private void cbx_SearchAllDirectory_CheckedChanged(object sender,EventArgs e) {
+            save_Config("SearchAllDirectories",cbx_SearchAllDirectory.Checked.ToString());
+        }
+
+        private void tbx_searchPattern_TextChanged(object sender,EventArgs e) {
+            save_Config("PhotoFileType",tbx_searchPattern.Text);
+        }
+
+        private void btn_ReadFile_Click(object sender,EventArgs e) {
+            foreach(DataGridViewRow row in dgv_Main.Rows) {
+                string fileName = row.Cells["name_Column"].Value.ToString();
+                string filePath = row.Cells["path_Column"].Value.ToString();
+
+                ShellObject shellObject = ShellObject.FromParsingName(filePath + "\\" + fileName);
+                DateTime dateTime = shellObject.Properties.System.ItemDate.Value.Value;
+
+                row.Cells["createTime_Column"].Value = dateTime.ToString("yyyy-MM-dd HH:mm:ss");
+
+                string createTime = row.Cells["createTime_Column"].Value.ToString();
+                string modifyTime = row.Cells["modifyTime_Column"].Value.ToString();
+                if(createTime.Equals(modifyTime)==false) {
+                    row.Cells["createTime_Column"].Style.ForeColor = Color.Red;
+                }
+            }
+        }
+
+        private void dgv_Main_CellClick(object sender,DataGridViewCellEventArgs e) {
+            if(e.RowIndex < 0) {
+                return;
+            }
+
+            directory_ContextMenuStrip.Show(dgv_Main,dgv_Main.PointToClient(MousePosition));
+            toolMenu_ImageName.Text = dgv_Main.Rows[e.RowIndex].Cells["name_Column"].Value.ToString();
         }
     }
 }
